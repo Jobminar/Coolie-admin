07fdb797de38cf392fbd1cf09b320c0b2e585c03
Anilvarmathirumalaraju <anilvarmathirumalaraju@gmail.com>
2024-05-27 18:11:21 +0530

    chnages in banners and layouts

--- Admin/src/components/Sections/Banners/Banners.jsx
+++ Admin/src/components/Sections/Banners/Banners.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import './banners.css'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import kitchen from '../../../assets/images/kitchen.png'
+import cleaning from '../../../assets/images/cleaning.png'
 
 const Banners = () => {
   return (
-    <div>Banners</div>
+     <>
+      
+     <div className='banners'>
+         <h1>Banners</h1>
+         <div className='banner-buttos'>
+              <div className='delete-button'>
+                  Delete 
+                  <DeleteOutlineOutlinedIcon/>
+              </div>
+              <div className='edit-button'>
+                   <EditOutlinedIcon/>
+              </div>
+         </div>
+         <div className='main-banners'>
+            <div className='Kitchen-cleaning'> 
+               <img src={kitchen} alt='kitchen'/>
+
+               <p>Kitchen cleaning
+               </p>
+               <div className='edit'>
+                     <EditOutlinedIcon/>
+               </div>
+            </div>
+            <div className='cleaning'>
+               <img src={cleaning} alt='cleaning'/>
+               <p>cleaning</p>
+            </div>
+            <div className='clean-edit'>
+                     <EditOutlinedIcon/>
+               </div>
+
+         </div>
+     </div>
+     </>
   )
 }
 
